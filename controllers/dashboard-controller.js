44aa05600cead12910eaab5da80bcd77af00c283
Marcos Oliveira Gomes <115948731+marcosogg@users.noreply.github.com>
2023-09-02 19:38:13 +0100

    Baseline 1 complete

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,6 +1,64 @@
 import { station } from "../models/station.js";
 import { reading } from "../models/reading.js";
 
+function windSpeedToBeaufort(windSpeed) {
+  const scales = {
+    1: { beaufort: 0, label: 'Calm' },
+    5: { beaufort: 1, label: 'Light Air' },
+    11: { beaufort: 2, label: 'Light Breeze' },
+    19: { beaufort: 3, label: 'Gentle Breeze' },
+    28: { beaufort: 4, label: 'Moderate Breeze' },
+    38: { beaufort: 5, label: 'Fresh Breeze' },
+    49: { beaufort: 6, label: 'Strong Breeze' },
+    61: { beaufort: 7, label: 'Near Gale' },
+    74: { beaufort: 8, label: 'Gale' },
+    88: { beaufort: 9, label: 'Severe Gale' },
+    102: { beaufort: 10, label: 'Strong Storm' },
+    117: { beaufort: 11, label: 'Violent Storm' }
+  }
+
+  if (Object.keys(scales).includes(windSpeed)) {
+    return scales[windSpeed];
+  }
+  return { beaufort: 'N/A', label: 'Out of scale' }
+}
+
+const getWindDirectionCompass = (windSpeed) => {
+  if(windSpeed >= 11.25 && windSpeed <= 33.75){
+    return 'NNE'
+  }else if(windSpeed >= 33.75 && windSpeed <= 56.25){
+    return 'NE'
+  }else if(windSpeed  >= 56.25 && windSpeed <= 78.75){
+    return 'ENE'
+  }else if(windSpeed  >= 78.75 && windSpeed <= 101.25){
+    return 'E'
+  }else if(windSpeed  >= 101.25 && windSpeed <= 123.75){
+    return 'ESE'
+  }else if(windSpeed  >= 123.75 && windSpeed <= 146.25){
+    return 'SE'
+  }else if(windSpeed  >= 146.25 && windSpeed <= 168.75){
+    return 'SSE'
+  }else if(windSpeed  >= 168.75 && windSpeed <= 191.25){
+    return 'S'
+  }else if(windSpeed  >= 191.25 && windSpeed <= 213.75){
+    return 'SSW'
+  }else if(windSpeed  >= 213.75 && windSpeed <= 236.25){
+    return 'SW'
+  }else if(windSpeed  >= 236.25 && windSpeed <= 258.75){
+    return 'WSW'
+  }else if(windSpeed  >= 258.75 && windSpeed <= 281.25){
+    return 'W'
+  }else if(windSpeed  >= 281.25 && windSpeed <= 303.75){
+    return 'WNW'
+  }else if(windSpeed  >= 303.75 && windSpeed <= 326.25){
+    return 'NW'
+  }else if(windSpeed  >= 326.25 && windSpeed <= 348.75){
+    return 'NNW'
+  }else if(windSpeed <= 348.75 && windSpeed >= 11.25){
+    return 'N'
+  }
+}
+
 export const dashboardController = {
   async index(request, response) {
     const viewData = {
@@ -14,51 +72,95 @@ export const dashboardController = {
   async addStation(request, response) {
     const newStation = {
       title: request.body.title,
+      latitude: request.body.latitude,
+      longitude: request.body.longitude,
     }
     console.log(`adding station ${newStation.title}`);
-    const _station = await station.addStation(newStation);
+    await station.addStation(newStation);
     response.redirect("/dashboard");
   },
   async addReadingForm(request, response) {
     const station_id = request.params.stationId
+    let maxTemperature = null;
+    let minTemperature = null;
+    let maxWind = null;
+    let minwind = null;
+
     let readings = await reading.getAllReadings(station_id)
     readings = readings.map((item) => {
       switch (item.code) {
         case 100:
           item.codeText = 'Clear';
+          item.icon = 'fas fa-sun';
           break;
         case 200:
           item.codeText = 'Partial Clouds';
+          item.icon = 'fas fa-cloud-sun';
           break;
         case 300:
           item.codeText = 'Cloudy';
+          item.icon = 'fas fa-cloud';
           break;
         case 400:
           item.codeText = 'Light Showers';
+          item.icon = 'fas fa-cloud-rain';
           break;
         case 500:
           item.codeText = 'Heavy Showers';
+          item.icon = 'fas fa-cloud-showers-heavy';
           break;
         case 600:
           item.codeText = 'Rain';
+          item.icon = 'fas fa-cloud-showers-heavy';
           break;
         case 700:
           item.codeText = 'Snow';
+          item.icon = 'fas fa-snowflake';
           break;
         case 800:
           item.codeText = 'Thunder';
+          item.icon = 'fas fa-bolt';
           break;
         default:
-        // Do nothing
+          item.codeText = '';
+          item.icon = 'fas fa-question';
+          break;
+      }
+
+      item.temperatureFahrenheit = item.temperature * 9 / 5 + 32;
+      const resultbft = windSpeedToBeaufort(item.windSpeed)
+      item.beaufort = resultbft['beaufort']
+      item.labelBeaufort = resultbft['label']
+      item.fellsLike = 13.12 + 0.6215 * item.temperature - 11.37 * (item.windSpeed * 0.16) + 0.3965 * item.temperature * (item.windSpeed * 0.16)
+      item.windDirectionCompass = getWindDirectionCompass(item.windDirection)
+      if (item.temperature > maxTemperature || maxTemperature == null) {
+        maxTemperature = item.temperature
+      }
+      if (item.temperature < minTemperature || minTemperature == null) {
+        minTemperature = item.temperature
+      }
+      if (item.windSpeed > maxWind || maxWind == null) {
+        maxWind = item.windSpeed
+      }
+      if (item.windSpeed < minwind || minwind == null) {
+        minwind = item.windSpeed
       }
       return item;
-    });
+    }); 
+
     readings.reverse()
-    readings[0].temperatureFahrenheit = readings[0].temperature * 9 / 5 + 32;
+    
     const viewData = {
       title: "Add Reading",
       readings: readings.slice(1),
-      last_reading: readings[0],
+      last_reading: {
+        // spread operator
+        ...readings[0],
+        maxTemperature: maxTemperature,
+        minTemperature: minTemperature,
+        maxWind: maxWind,
+        minwind: minwind
+      },
       station: await station.getStationById(station_id),
       station_id
     };
